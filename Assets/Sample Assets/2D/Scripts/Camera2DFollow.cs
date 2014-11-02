507681824f43d6f48c34d9a893d5660337479c95
frulica <juricacv@gmail.com>
2014-11-02 20:46:27 +0100

    firefly init

--- Assets/Sample Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Sample Assets/2D/Scripts/Camera2DFollow.cs
@@ -8,6 +8,7 @@ public class Camera2DFollow : MonoBehaviour {
 	public float lookAheadFactor = 3;
 	public float lookAheadReturnSpeed = 0.5f;
 	public float lookAheadMoveThreshold = 0.1f;
+	public float offsetY = 0.0f;
 	
 	float offsetZ;
 	Vector3 lastTargetPosition;
@@ -36,6 +37,7 @@ public class Camera2DFollow : MonoBehaviour {
 		}
 		
 		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		aheadTargetPos.y = aheadTargetPos.y + offsetY;
 		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
 		
 		transform.position = newPos;
---
+++ Assets/Steer2D/Scripts/Flee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Steer2D
+{
+    public class Flee : SteeringBehaviour
+    {
+        public Transform FleeFrom;
+        public float FleeRadius = 1;
+        public bool DrawGizmos = false;
+
+        public override Vector2 GetVelocity()
+        {
+			float distance = Vector3.Distance(transform.position, FleeFrom.position);
+
+            if (distance < FleeRadius) 
+				return -((((Vector2)FleeFrom.position - (Vector2)transform.position).normalized * agent.MaxVelocity) - agent.CurrentVelocity);
+            else
+                return Vector2.zero;
+        }
+
+        void OnDrawGizmos()
+        {
+            if (DrawGizmos)
+            {
+                Gizmos.color = Color.blue;
+				Gizmos.DrawWireSphere(FleeFrom.position, FleeRadius);
+            }
+        }
+    }
+}
