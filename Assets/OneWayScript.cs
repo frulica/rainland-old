da77b5a03487ef8ffb608a0268553c4387c63b29
Jurica Cvetko <juricacv@gmail.com>
2014-11-12 18:12:30 +0100

    one-way platform

---
+++ Assets/OneWayScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayScript : MonoBehaviour {
+
+	public BoxCollider2D platform;
+	public bool oneWay = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		platform.enabled = !oneWay; 
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		oneWay = true;
+	}
+	
+	void OnTriggerStay2D(Collider2D other)
+	{
+		oneWay = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		oneWay = false;
+	}
+
+}
